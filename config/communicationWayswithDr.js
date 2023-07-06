de34ad3f85b45cb9b07478950f81a98bb601490a
MariamMahfouz19 <78443450+MariamMahfouz19@users.noreply.github.com>
2023-07-06 20:33:43 +0300

    communicationWayswithDr.js

--- config/communicationWayswithDr.js
+++ config/communicationWayswithDr.js
@@ -4,6 +4,7 @@ const Ways = require('../model/communicationWayswithDr');
 //Add Communication Ways Info.
 const store = (req, res, next) => { 
     let CommunicationWays = new Ways({
+        d_id:req.body.d_id,
         WhatsApp_Link: req.body.WhatsApp_Link,
         Telegram_Link: req.body.Telegram_Link,
         FaceBook_Link: req.body.FaceBook_Link,
@@ -37,7 +38,7 @@ const updateInfo = (req, res, next) => {
         Gmail_Link: req.body.Gmail_Link || Ways.Gmail_Link
     }
     
-    Ways.findByIdAndUpdate(ID, {$set: updatedData})
+    Ways.findOneAndUpdate(ID, {$set: updatedData})
     .then(() => {
     res.json({
         message: 'Doctor\'s Communication Ways Info. updated successfully!'
@@ -63,7 +64,7 @@ const destroy = (req, res, next) => {
     }
     
     
-    Ways.findByIdAndRemove(ID, {$set: deletedData})
+    Ways.findOneAndRemove(ID, {$set: deletedData})
     .then(() => {
     res.json({
         message: 'One Or More Of Your (Doctor) Communication Ways Info. Deleted successfully!'
@@ -74,10 +75,27 @@ const destroy = (req, res, next) => {
                 message: 'An error Occured!'
         })
     })
-    }
+}
+// show communication ways info
+const index= (req, res, next) => {
+    
+    Ways.find({d_id : req.body.d_id})
+    .then(response => {
+        res.json({
+          response
+    })  
+})
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+    })
+}
+
 
     module.exports = {
         store,
         updateInfo,      //update a Doctor Communication Ways Information
-        destroy     //delete One Or More Of Communication Ways of doctor.
+        destroy,     //delete One Or More Of Communication Ways of doctor.
+        index
     }
