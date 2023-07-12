da25ad3adcb14b64ac6095cf5fbf715b7cebac83
MariamMahfouz19 <78443450+MariamMahfouz19@users.noreply.github.com>
2023-07-12 22:12:15 +0300

    times_of_drugs.js

--- routes/times_of_drugs.js
+++ routes/times_of_drugs.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/all-drugs-for-patient', times_of_drugs_Controller.index)
+router.post('/all-drugs-for-patient', times_of_drugs_Controller.index)
 router.post('/all-drugs-for-doctor', times_of_drugs_Controller.show_To_Doctor)
 router.post('/drug-by-name', times_of_drugs_Controller.show)
 router.post('/store-drug', times_of_drugs_Controller.store)
